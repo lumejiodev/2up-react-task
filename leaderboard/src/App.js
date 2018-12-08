58ebdb9e6d9a55d5231236700dd82ba67c009a50
anastasia <korsikov009@gmail.com>
2018-12-08 14:51:22 +0300

    changed structure of LeaderCard

--- leaderboard/src/App.js
+++ leaderboard/src/App.js
@@ -3,6 +3,7 @@ import './Common-styles.css';
 import Header from './Header/Header';
 import ParticipantCard from './ParticipantCard/ParticipantCard';
 import LeaderCard from './LeaderCard/LeaderCard';
+import cup from './LeaderCard/cup.svg';
 
 class App extends Component {
   render() {
@@ -19,9 +20,9 @@ class App extends Component {
           <Header title='Лидеры'/>
 
           <div className='leadersContainer'>
-            <LeaderCard name='Пётр Иванов' score='8901 очков'/>
-            <LeaderCard name='Максим Кальченко' score='7890 очков'/>
-            <LeaderCard name='Алёна Михалкова' score='7801 очков'/>
+            <LeaderCard name='Пётр Иванов' score='8901 очков' img={cup}/>
+            <LeaderCard name='Максим Кальченко' score='7890 очков' img={cup}/>
+            <LeaderCard name='Алёна Михалкова' score='7801 очков' img={cup}/>
             <LeaderCard name='Виталий Янкевич' score='7699 очков'/>
             <LeaderCard name='Богдан Емельянов' score='5641 очков'/>
             <LeaderCard name='Владимир Жванецкий' score='5278 очков'/>
--- leaderboard/src/LeaderCard/LeaderCard.js
+++ leaderboard/src/LeaderCard/LeaderCard.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './LeaderCard.css';
 
+
 const leaderCard = (props) => {
     return (
-        <div className='LeaderCard'>
-            <p className='Leader-name'>{ props.name }</p>
-            <p className='Leader-score'>{ props.score }</p>
+        <div className='LeaderCard__container'>
+            <div className='LeaderCard'>
+                <p className='Leader-name'>{ props.name }</p>
+                <p className='Leader-score'>{ props.score }</p>
+            </div>
+            <div className='LeaderCard__image'>
+                <img src={props.img} alt='cup'/>
+            </div>
         </div>
     )
 }
